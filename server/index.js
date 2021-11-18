aff26a515ae6afc5e8f5438bed2692c4f53d5a95
Michael Hardcastle <maaicle@gmail.com>
2021-11-18 15:43:11 -0700

    test

--- server/index.js
+++ server/index.js
@@ -28,11 +28,11 @@ app.get('/index.css', (req, res) => {
     rollbar.info('css file loaded successfully')
 });
 
-app.get('/main', (req, res) => {
+app.get('/main.js', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/main.js'));
 });
 
-app.get('/objobs', (req, res) => {
+app.get('/object-observer.js', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/object-observer.js'));
 });
 
@@ -40,20 +40,20 @@ app.get('/pics/hkdrywall-curve.png', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/pics/HKdrywall-curve.png'));
 });
 
-app.get('/stonewall', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pics/stonewall-bw.jpg'));
+app.get('/pics/stonewall-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/stonewall-bw.jpg'));
 });
 
-app.get('/elevator', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pics/elevator-bw.jpg'));
+app.get('/pics/elevator-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/elevator-bw.jpg'));
 });
 
-app.get('/painthole', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pics/paint-hole-window-bw.jpg'));
+app.get('/pics/paint-hole-window-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/paint-hole-window-bw.jpg'));
 });
 
-app.get('/studwall', (req, res) => {
-    res.sendFile(path.join(__dirname, '../pics/stud-wall-bw.jpg'));
+app.get('/pics/stud-wall-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/stud-wall-bw.jpg'));
 });
 
 app.use(rollbar.errorHandler());
