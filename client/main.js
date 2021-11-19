821bfd1f2dbe64aadf2fbdea0f541fcc514f4ab1
Michael Hardcastle <maaicle@gmail.com>
2021-11-18 20:26:19 -0700

    Reverting to aff26a515ae6afc5e8f5438bed2692c4f53d5a95

--- client/main.js
+++ client/main.js
@@ -1,20 +1,15 @@
-// const axios = require('axios');
-// const port = process.env.PORT || 4007;
-
 //Get elements
 const logo = document.querySelector('.logo');
 
 // Calls the createObserver function once the page is loaded.
 window.addEventListener("load", event => createObserver(), false);
 
-// const testCall = event => {
-//     console.log('Front End Works')
-
-//     axios.get(`https://hardkastledrywall.herokuapp.com/rollbartest`).then(res => {
-//         console.log(res.data);
-//     })
-//     .catch(err => console.log(err.data))
-// }
+const testCall = event => {
+    console.log('Front End Works')
+    // axios.get('/rollbartest').then(res => {
+        // console.log(res);
+    // })
+}
 
 logo.addEventListener('click', testCall);
 
---
+++ server/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '9648c58445df43a8b17bcf72c3464a74',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+app.get('/rollbartest', (req, res) => {
+    rollbar.log("This is a new test");
+    console.log("This is a new test");
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+    rollbar.info('html file loaded successfully');
+});
+
+app.get('/index.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.css'));
+    rollbar.info('css file loaded successfully')
+});
+
+app.get('/main.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/main.js'));
+});
+
+app.get('/object-observer.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/object-observer.js'));
+});
+
+app.get('/pics/hkdrywall-curve.png', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/HKdrywall-curve.png'));
+});
+
+app.get('/pics/stonewall-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/stonewall-bw.jpg'));
+});
+
+app.get('/pics/elevator-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/elevator-bw.jpg'));
+});
+
+app.get('/pics/paint-hole-window-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/paint-hole-window-bw.jpg'));
+});
+
+app.get('/pics/stud-wall-bw.jpg', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/pics/stud-wall-bw.jpg'));
+});
+
+app.use(rollbar.errorHandler());
+
+const port = process.env.PORT || 4007;
+
+app.listen(port, () => {
+    console.log(`Docked at port ${port}`);
+});
